1c893d33aeac5f7718a4378a422fcac25231e1a3
GuiLLeRm0 <gdavid.ptorrez@gmail.com>
2015-03-29 23:08:08 -0400

    Three commit

--- app/src/main/java/bo/umsa/acelerometro/acelerometro/MainActivity.java
+++ app/src/main/java/bo/umsa/acelerometro/acelerometro/MainActivity.java
@@ -23,6 +23,8 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
     private long lastUpdate = 0;
     private float last_x, last_y, last_z;
     private static final int SHAKE_THRESHOLD = 600;
+
+    private SensorEvent sensorEvent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,9 +36,37 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
     }
 
     @Override
-    public void onSensorChanged(SensorEvent event){
+    public void onSensorChanged(SensorEvent event) {
 
+        Sensor mySensor = sensorEvent.sensor;
+
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            long curTime = System.currentTimeMillis();
+
+            if((curTime - lastUpdate) > 100){
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+
+                float speed = Math.abs(x + y +z - last_x - last_y - last_z)/ diffTime * 10000;
+
+                if(speed  > SHAKE_THRESHOLD){
+
+                }
+
+                last_x = x;
+                last_y = y;
+                last_z = z;
+            }
+        }
     }
+
+
+
+
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy){
