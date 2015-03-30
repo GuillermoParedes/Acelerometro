82d15c5d2a554c80ab6d2494b1b4abd56a5756f0
GuiLLeRm0 <gdavid.ptorrez@gmail.com>
2015-03-29 23:44:10 -0400

    Three commit

--- app/src/main/java/bo/umsa/acelerometro/acelerometro/MainActivity.java
+++ app/src/main/java/bo/umsa/acelerometro/acelerometro/MainActivity.java
@@ -13,6 +13,14 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity implements SensorEventListener {
@@ -54,7 +62,7 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
                 float speed = Math.abs(x + y +z - last_x - last_y - last_z)/ diffTime * 10000;
 
                 if(speed  > SHAKE_THRESHOLD){
-
+                    getRandomNumber();
                 }
 
                 last_x = x;
@@ -64,15 +72,85 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
         }
     }
 
-
-
-
-
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy){
 
     }
+    private void getRandomNumber(){
+        ArrayList numbersGenerated = new ArrayList();
 
+        for (int i = 0; i < 6; i++){
+            Random randNumber = new Random();
+            int iNumber = randNumber.nextInt(48)+1;
+
+            if(!numbersGenerated.contains(iNumber)){
+                numbersGenerated.add(iNumber);
+            } else {
+                i--;
+            }
+        }
+
+        TextView text = (TextView)findViewById(R.id.number_1);
+        text.setText(""+numbersGenerated.get(0));
+
+        text = (TextView)findViewById(R.id.number_2);
+        text.setText(""+numbersGenerated.get(1));
+
+        text = (TextView)findViewById(R.id.number_3);
+        text.setText(""+numbersGenerated.get(2));
+
+        text = (TextView)findViewById(R.id.number_4);
+        text.setText(""+numbersGenerated.get(3));
+
+        text = (TextView)findViewById(R.id.number_5);
+        text.setText(""+numbersGenerated.get(4));
+
+        text = (TextView)findViewById(R.id.number_6);
+        text.setText(""+numbersGenerated.get(5));
+
+        FrameLayout ball1 = (FrameLayout) findViewById(R.id.ball_1);
+        ball1.setVisibility(View.INVISIBLE);
+
+        FrameLayout ball2 = (FrameLayout) findViewById(R.id.ball_2);
+        ball2.setVisibility(View.INVISIBLE);
+
+        FrameLayout ball3 = (FrameLayout) findViewById(R.id.ball_3);
+        ball3.setVisibility(View.INVISIBLE);
+
+        FrameLayout ball4 = (FrameLayout) findViewById(R.id.ball_4);
+        ball4.setVisibility(View.INVISIBLE);
+
+        FrameLayout ball5 = (FrameLayout) findViewById(R.id.ball_5);
+        ball5.setVisibility(View.INVISIBLE);
+
+        FrameLayout ball6 = (FrameLayout) findViewById(R.id.ball_6);
+        ball6.setVisibility(View.INVISIBLE);
+
+        Animation a = AnimationUtils.loadAnimation(this, R.anim.move_down_ball_first);
+        ball6.setVisibility(View.VISIBLE);
+        ball6.clearAnimation();
+        ball6.startAnimation(a);
+
+        ball5.setVisibility(View.VISIBLE);
+        ball5.clearAnimation();
+        ball5.startAnimation(a);
+
+        ball4.setVisibility(View.VISIBLE);
+        ball4.clearAnimation();
+        ball4.startAnimation(a);
+
+        ball3.setVisibility(View.VISIBLE);
+        ball3.clearAnimation();
+        ball3.startAnimation(a);
+
+        ball2.setVisibility(View.VISIBLE);
+        ball2.clearAnimation();
+        ball2.startAnimation(a);
+
+        ball1.setVisibility(View.VISIBLE);
+        ball1.clearAnimation();
+        ball1.startAnimation(a);
+    }
     protected void onPause(){
         super.onPause();
         senSensorManager.unregisterListener(this);
